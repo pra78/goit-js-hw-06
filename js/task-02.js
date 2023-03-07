5ea5376f990df6085fe240d4b2d4acf6db3ded5b
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-07 23:10:59 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elementContainer = document.querySelector("ul#ingredients")
+
+const elements = ingredients.map(ingredient => {
+  const ingredientLi = document.createElement('li');
+  ingredientLi.textContent = ingredient;
+  ingredientLi.classList.add('item');
+  return ingredientLi;
+})
+
+elementContainer.append(...elements);
+// console.log(elements)
