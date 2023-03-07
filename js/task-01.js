af99e2424cdaeec2219649aad4e35bdb4d357fad
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-07 21:29:11 +0200

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,8 @@
-// Напиши скрипт, який:
+const ulWithId = document.querySelector("ul#categories");
+console.log(`Number of categories: ${ulWithId.childElementCount}`);
 
-// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
-// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
-
-// В результаті, в консолі будуть виведені наступні повідомлення.
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
-const categoriesEl = document.querySelector('#categories');
-console.log(`Number of categories: ${categoriesEl.c}`);
+const listWithClass = document.querySelectorAll(".item");
+listWithClass.forEach(listItem => {
+    console.log(`Category: ${listItem.children[0].textContent}`);
+    console.log(`Elements: ${listItem.children[1].childElementCount}`);
+})
