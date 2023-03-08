cb7f09da85a2d516cffd50677253c331d8e66746
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-08 22:59:16 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("#name-input");
+const nameLabel = document.querySelector("#name-output");
+
+input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    if (event.currentTarget.value) {
+        nameLabel.textContent = event.currentTarget.value;
+    } else {
+        nameLabel.textContent = "Anonymous";
+    }
+    
+}
