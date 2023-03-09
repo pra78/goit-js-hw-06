445f67b452b5fdec82703969fd7a7ab67b8f5724
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-09 14:28:26 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const emailValue = formElements.email.value;
+    const passwordValue = formElements.password.value;
+    
+    const formFilled = { email: emailValue, password: passwordValue };
+
+    console.log(formFilled);
+
+    form.reset();
+}
