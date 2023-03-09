c7437454e5d14e778db6f229deb7874e6c0fc5f3
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-09 16:41:26 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector('#controls');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+createButton.addEventListener('click', onCreateButtonClick);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const boxes = [];
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${width}px`;
+    box.style.height = `${height}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+
+    width += 10;
+    height += 10;
+  }
+
+  return boxes;
+}
+
+function onCreateButtonClick() {
+  const amount = Number(controls.querySelector('input').value);
+  const boxes = createBoxes(amount);
+  boxesDiv.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
