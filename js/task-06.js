d88fed75bc1cda0c2fd0d617c2127718d5e31f4d
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-10 18:28:51 +0200

    task 6 condition corrected

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,11 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-    if (event.currentTarget.value.length < input.dataset.length) {
-        input.classList.add('invalid');
-        input.classList.remove('valid');
-    } else {
+    if (Number(event.currentTarget.value.length) === Number(input.dataset.length)) {
         input.classList.add('valid');
         input.classList.remove('invalid');
+    } else {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
     }
 }
